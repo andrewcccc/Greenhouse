ec2e872ff744d12c86c4fb65a835de6405f2eebe
Andrew Chao <cusan@ualberta.ca>
2022-12-27 16:02:50 -0500

    Capstone project

--- app/src/main/java/com/example/greenhouse/ui/gallery/ApiInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.greenhouse.ui.gallery;
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-
-public interface ApiInterface {
-    @GET("graph2.php")  //change this
-    Call<Chart> init();
-}
